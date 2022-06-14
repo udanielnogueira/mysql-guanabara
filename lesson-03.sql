dd67b75ac46b2488ecd33628801a3b6424271beb
udanielnogueira <danielsilvangr@gmail.com>
2022-06-14 16:59:33 -0300

    Update file

--- lesson-03.sql
+++ lesson-03.sql
@@ -1,10 +1,17 @@
 # NÃO PRECISO DO "ID" POR CAUSA DO AUTO_INCREMENT
-# OU POSSO COLOCAR, COM O VALUE SENDO DEFAULT
+# OU POSSO COLOCAR COM O VALUE SENDO DEFAULT
 
 insert into pessoas
-(id, nome, nascimento, sexo, peso, altura, nacionalidade)
+# (id, nome, nascimento, sexo, peso, altura, nacionalidade) PODE SER OMITIDO SE A ORDEM FOR A MESMA
 values
-(default, 'Ane', '1997-10-30', 'F', '54.0', '1.60', 'Brasil');
+(default, 'Danilo', '1995-05-30', 'M', '87.0', '1.80', 'Portugal'),
+(default, 'Carlos', '1998-07-10', 'M', '70.0', '1.80', 'Argentina');
 
 # EXIBE OS REGISTROS
 select * from pessoas; 
+
+# COMANDOS DDL (DATA DEFINITION LANGUAGE)
+# CREATE DATABASE CREATE TABLE
+
+# COMANDOS DML (DATA MANIPULATION LANGUAGE)
+# INSERT INTO
