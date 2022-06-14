81cb3c714a328dfa9e2116fe3abd47dae6a8025a
udanielnogueira <danielsilvangr@gmail.com>
2022-06-14 15:44:14 -0300

    Update file

--- lesson-02.sql
+++ lesson-02.sql
@@ -1,4 +1,4 @@
-create database cadastro
+create database cadastro02
 
 # VOU ADICIONAR DUAS CONSTRAINTS
 # VOU TER UMA CODIFICAÇÃO DE COLLATE
@@ -7,19 +7,20 @@ create database cadastro
 default character set utf8 
 default collate utf8_general_ci;
 
-use cadastro;
+use cadastro02;
 
 # "DROP DATABASE X" FAZ APAGAR UM BD
 
 # USO "NOT NULL" PARA OBRIGAR QUE DEVA POSSUIR UM VALOR
+# "AUTO_INCREMENT" VAI INCREMENTAR A CADA NOVO REGISTRO
 
 create table pessoas (
+id int not null auto_increment, 
 nome varchar(30) NOT NULL,
 nascimento date, 
 sexo enum('M','F'), # SÓ ACEITO M OU F
 peso decimal(5,2), # DESSES 5, 2 DÍGITOS FICARÃO APÓS A VÍRGULA
 altura decimal(3,2),
-nacionalidade varchar(20) DEFAULT 'Brasil' # SE NÃO DIGITAR, POR PADRÃO SERÁ BRASIL
+nacionalidade varchar(20) default 'Brasil', # SE NÃO DIGITAR, POR PADRÃO SERÁ BRASIL
+primary key(id)
 ) default charset = utf8;
-
-describe pessoas;
