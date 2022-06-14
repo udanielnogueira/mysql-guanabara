897dbe09200d0837cf508b65b494d08b40f90500
udanielnogueira <danielsilvangr@gmail.com>
2022-06-14 17:24:45 -0300

    Update file

--- lesson-04.sql
+++ lesson-04.sql
@@ -1,4 +1,17 @@
-desc pessoas; # DESC OU DESCRIBE
+# "DESC" OU "DESCRIBE"
+desc pessoas; 
 
 alter table pessoas
 add column profissao varchar(10);
+
+alter table pessoas
+drop column profissao;
+
+# O "COLUMN" É ADICIONAL
+alter table pessoas
+add profissao varchar(10) after nome;
+
+# PRA ADICIONAR COMO PRIMEIRO
+# "ADD COLUMN CODIGO INT FIRST"
+
+select * from pessoas;
